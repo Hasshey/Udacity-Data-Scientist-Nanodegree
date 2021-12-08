bc035fb2a9f8c606a2cda47e9a3d2bac868cbd94
Harshith <harshith.allamsetti@gmail.com>
2021-12-07 16:18:33 -0800

    Added code to train_classifier.py file

--- Project 2 - Disaster Response pipeline/models/train_classifier.py
+++ Project 2 - Disaster Response pipeline/models/train_classifier.py
@@ -1,24 +1,144 @@
 import sys
+import nltk
+import pickle
+nltk.download(['punkt', 'wordnet'])
 
+import pandas as pd
+from sqlalchemy import create_engine
+
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
 
 def load_data(database_filepath):
-    pass
+    '''
+        Load the cleaned input data from the database and obtain the features/target for the model
+
+        Parameters:
+                database_filepath: The filepath in the database to retrieve the dataframe
+
+        Returns:
+                X: Features of the model
+                Y: Target of the model
+                category_names: Names of different categories
+         
+    '''
+
+    #Create the SQL enginer connection and retrieve the cleaned up dataframe
+    engine = create_engine('sqlite:///{}'.format(database_filepath))
+    df = pd.read_sql_table('InsertTableName', con=engine)
+
+    #Create the Features and Target for the model
+    X = df['message']
+    Y = df.drop(columns = ['id', 'message', 'original', 'genre'])
+
+    #Get the names of different categories
+    category_names = Y.columns
+
+    return X, Y, category_names
+
 
 
 def tokenize(text):
-    pass
+    '''
+
+        Tokenizer function for the input messages to be used in the model
+
+        Parameters:
+                text: Raw input text from the input dataframe
+
+        Returns:
+                clean_tokens: A list of clean tokens
+
+    '''
+
+    # Tokenize the input text 
+    tokens = word_tokenize(text)
+
+    # Create a lemmatizer for the tokens created
+    lemmatizer = WordNetLemmatizer()
+    
+    clean_tokens = []
+    
+    # Iterate through the tokens
+    for token in tokens:
+
+        # Lemmatize the tokens, normalize the case and remove leading/trailing spaces
+        clean_token = lemmatizer.lemmatize(token).lower().strip()
+        clean_tokens.append(clean_token)
+        
+    return clean_tokens
 
 
 def build_model():
-    pass
+    '''
+
+        Builds Classifier and tunes the model using GridSearchCV
+        
+        Parameters:
+                None
+        Returns:
+                cv: Classifier
+    
+    '''
+
+    pipeline = Pipeline([
+    ('vect', CountVectorizer(tokenizer=tokenize, analyzer = 'char')),
+    ('tfidf', TfidfTransformer()),
+    ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+
+    parameters = {
+        'clf__estimator__n_estimators' : [50, 100]
+    }
+    
+    cv = GridSearchCV(pipeline, param_grid=parameters, verbose=3)
+    
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    '''
+    
+        Evaluates the model's performance and display classification report
 
+        Parameters: 
+            model: Classifier
+            X_test: Test data for X
+            Y_test: Test data for Y
+            category_names: names for different categories
+
+        Returns:
+            None
+
+    '''
+
+    y_pred = model.predict(X_test)
+    for index, column in enumerate(Y_test):
+        print(column, classification_report(Y_test[column], y_pred[:, index]))
 
 def save_model(model, model_filepath):
-    pass
+    '''
+
+        Export the model as a pickle file
+
+        Parameters:
+                model: Classifier
+                model_filepath: Path of the pickle file
+
+        Returns:
+                None
+    '''
+
+    pickle.dump(model, open(model_filepath), 'wb')
 
 
 def main():
